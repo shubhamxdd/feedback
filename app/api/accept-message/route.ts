9674ee4f2df7c384e913001f0c378e5f3f188cef
shubhamxdd <ssisodia128@gmail.com>
2024-06-22 20:20:19 +0530

    Add API routes for accepting and retrieving messages

---
+++ app/api/accept-message/route.ts
@@ -0,0 +1,114 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { db } from "@/lib/db";
+import UserModel from "@/models/User";
+import { NextResponse } from "next/server";
+import { User } from "next-auth";
+
+export async function POST(req: Request) {
+  await db();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  const { allowingMessages } = await req.json();
+
+  try {
+    const updatedUser = UserModel.findByIdAndUpdate(
+      userId,
+      {
+        isAllowingNewMessages: allowingMessages,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "isAllowingNewMessages status is updated successfully.",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: Request) {
+  await db();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  try {
+    const existingUser = await UserModel.findById(userId);
+    if (!existingUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: existingUser.isAllowingNewMessages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
